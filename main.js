8312902d0537480676675e5b830f8875a46bce23
Fewsmk <161725803+Oineh@users.noreply.github.com>
2025-08-08 16:31:52 -0300

    checked

--- main.js
+++ main.js
@@ -24,12 +24,24 @@ function aumentaTamanho(){
 const campoSenha = document.querySelector('#campo-senha');
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '1234567890';
+const simbolos = '!@#$%¨&*()_'
+
 
 geraSenha();
 
 function geraSenha(){
-    let senha = '';
+    let alfaebeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+     if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+     if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
     for (let i = 0 ; i < tamanhoSenha; i++){
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
     }
